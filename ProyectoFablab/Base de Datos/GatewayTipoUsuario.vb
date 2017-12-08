a1da26d9218e3e99023d363e3ed78d3751642c9c
alumno <alumno@2DAMB24>
2017-12-08 17:54:32 +0100

    Clase GatewayTipoUsuario

---
+++ ProyectoFablab/Base de Datos/GatewayTipoUsuario.vb
@@ -0,0 +1,116 @@
+﻿Imports System.Data.SqlClient
+Public Class GatewayTipoUsuario
+    Private conexion As SqlConnection
+    Private comando As SqlCommand
+
+    Public Sub New(conexion As String)
+        Me.conexion = New SqlConnection(conexion)
+        comando = New SqlCommand()
+        comando.Connection = Me.conexion
+    End Sub
+
+    ''' <summary>
+    ''' Método que inserta un registro en la base de datos.
+    ''' </summary>
+    ''' <param name="tipo">El tipo de usuario.</param>
+    ''' <returns>Regresa el número de filas afectadas.</returns>
+    Public Function Insertar(tipo As String) As Integer
+
+        Dim consulta As String
+        Dim filasAfectadas As Integer
+
+        DatoNoValido(tipo.Trim(), "", "El campo tipo no puede estar vacío.")
+        consulta = "INSERT INTO TiposUsuario (tipo) VALUES(@tipo)"
+        Try
+            conexion.Open()
+            comando.Parameters.Add("@tipo", SqlDbType.VarChar)
+            comando.Parameters("@tipo").Value = tipo
+            comando.CommandText = consulta
+            filasAfectadas = comando.ExecuteNonQuery()
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion IsNot Nothing) Then
+                conexion.Close()
+                comando.Parameters.Clear()
+            End If
+        End Try
+
+        Return filasAfectadas
+    End Function
+
+    ''' <summary>
+    ''' Modifica un registro de la base de datos.
+    ''' </summary>
+    ''' <param name="id">Identificador del usuario.</param>
+    ''' <param name="tipo">El tipo de usuario para modificar.</param>
+    ''' <returns>Regresa el número de filas afectadas.</returns>
+    Public Function Actualizar(id As Integer, tipo As String) As Integer
+        Dim consulta As String
+        Dim filasAfectadas As Integer
+
+        DatoNoValido(id, 0, "El campo id no puede tener valor 0.")
+        DatoNoValido(tipo.Trim(), "", "El campo tipo no puede estar vacío.")
+        comando.Parameters.Add("@tipo", SqlDbType.VarChar)
+        comando.Parameters("@tipo").Value = tipo
+        consulta = "UPDATE TiposUsuario SET tipo=@tipo WHERE id=" & id
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            comando.ExecuteNonQuery()
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            If (conexion Is Nothing) Then
+                comando.Parameters.Clear()
+                conexion.Close()
+            End If
+        End Try
+        Return filasAfectadas
+    End Function
+
+    ''' <summary>
+    ''' Elimina un registro de la tabla
+    ''' </summary>
+    ''' <param name="id"></param>
+    ''' <returns>Regresa el número de filas afectadas.</returns>
+    Function Eliminar(id As Integer) As Integer
+
+        Dim consulta As String
+        Dim filasAfectadas As Integer
+        DatoNoValido(id, 0, "El campo id no puede tener valor 0")
+        consulta = "DELETE FROM tiposUsuario WHERE id=" & id
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            filasAfectadas = comando.ExecuteNonQuery()
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        End Try
+        Return filasAfectadas
+    End Function
+
+
+    ''' <summary>
+    ''' Muestra los tipos registrados en la base de datos.
+    ''' </summary>
+    ''' <returns>Regresa un objeto de tipo DataTable con todos los registros.</returns>
+    Function MostrarTipos() As DataTable
+
+        Dim consulta As String
+        Dim lectura As SqlDataReader
+        Dim resultado As New DataTable
+        consulta = "SELECT * FROM TiposUsuario"
+        Try
+            conexion.Open()
+            comando.CommandText = consulta
+            lectura = comando.ExecuteReader()
+            resultado.Load(lectura)
+        Catch ex As Exception
+            Throw New Exception(ex.Message, ex)
+        Finally
+            conexion.Close()
+        End Try
+        Return resultado
+    End Function
+End Class
