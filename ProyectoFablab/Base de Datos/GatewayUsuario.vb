419a1bce8fa59874262dfd182eb424f27fb59884
alumno <alumno@2DAMB24>
2017-12-08 02:29:22 +0100

    Cambios en GatewayUsuario

--- ProyectoFablab/Base de Datos/GatewayUsuario.vb
+++ ProyectoFablab/Base de Datos/GatewayUsuario.vb
@@ -1,5 +1,5 @@
 ﻿Imports System.Data.SqlClient
-Imports System.Text.RegularExpressions
+
 Public Class GatewayUsuario
     Private conexion As SqlConnection
     Private comando As SqlCommand
@@ -13,72 +13,37 @@ Public Class GatewayUsuario
     '******************************************************************************
     'FUNCIONES Y PROCEDIMIENTOS PRIVADOS
     '******************************************************************************
-    ''' <summary>
-    ''' Comprueba que el dato pasado como parámetro, no este vacío y no sea nulo.
-    ''' </summary>
-    ''' <param name="dato">Cadena a comprobar.</param>
-    ''' <param name="mensaje">Mensaje de error a mostrar cuando el dato está vacío o es nulo.</param>
-    Private Sub CampoVacio(dato As String, mensaje As String)
-        If dato.Trim().CompareTo("") = 0 Or dato Is Nothing Then
-            Throw New ArgumentException(mensaje)
-        End If
-    End Sub
-
-
-    ''' <summary>
-    ''' Comprueba si el dato pasado como parámetro, es un número de teléfono de 9 dígitos.
-    ''' </summary>
-    ''' <param name="telefono">Número de teléfono.</param>
-    Private Sub ComprobarTelefono(telefono As String)
-        Dim telefonoCorrecto As New Regex("[0-9]{9}")
-        If Not telefonoCorrecto.IsMatch(telefono) Then
-            Throw New ArgumentException("El teléfono debe contener 9 dígitos.")
-        End If
-    End Sub
-
-
-    ''' <summary>
-    ''' Comprueba si el dato pasado como parámetro, es un correo electrónico.
-    ''' </summary>
-    ''' <param name="correo">Correo elétronico.</param>
-    Private Sub ComprobarCorreo(correo As String)
-        Dim correoCorrecto As New Regex("^\w+@\w+.[a-z]{3}$")
-        If Not correoCorrecto.IsMatch(correo) Then
-            Throw New ArgumentException("Introduce un correo válido.")
-        End If
-    End Sub
-
 
     ''' <summary>
     ''' Procedimiento que obliga a introducir un número de teléfono, un correo o una de las dos formas de contacto.
     ''' </summary>
     ''' <param name="telefono">Numero de teléfono.</param>
     ''' <param name="correo">Correo eléctronico.</param>
-    Private Sub ContactoObligatorio(telefono As String, correo As String)
+    Private Sub ContactoObligatorio(ByVal telefono As String, ByVal correo As String)
+        Dim mensajeFono As String = "El teléfono debe contener 9 dígitos."
+        Dim mensajeCorreo As String = "Introduce un correo válido."
+        Dim patronFono As String = "[0-9]{9}"
+        Dim patronCorreo As String = "^\w+@\w+.[a-z]{3}$"
         If Not telefono.Trim().Equals("") And Not correo.Trim().Equals("") Then
-            ComprobarTelefono(telefono)
-            ComprobarCorreo(correo)
+            If Not ValidaCadena(telefono, patronFono) Then
+                Throw New ArgumentException(mensajeFono)
+            End If
+            If Not ValidaCadena(correo, patronCorreo) Then
+                Throw New ArgumentException(mensajeCorreo)
+            End If
         ElseIf Not telefono.Trim().Equals("") Then
-            ComprobarTelefono(telefono)
+            If Not ValidaCadena(telefono, patronFono) Then
+                Throw New ArgumentException(mensajeFono)
+            End If
         ElseIf Not correo.Trim().Equals("") Then
-            ComprobarCorreo(correo)
+            If Not ValidaCadena(correo, patronCorreo) Then
+                Throw New ArgumentException(mensajeCorreo)
+            End If
         Else
             Throw New ArgumentException("No hay ningún dato de contacto.")
         End If
     End Sub
 
-
-    ''' <summary>
-    ''' Función que comprueba si el dato pasado como parámetro, está vacío.
-    ''' </summary>
-    ''' <param name="cadena">Dato a comprobar.</param>
-    ''' <returns>Regresa true o false.</returns>
-    Private Function CampoEsNulo(cadena As String) As Boolean
-        If cadena.Trim().CompareTo("") = 0 Then
-            Return True
-        End If
-        Return False
-    End Function
 
     '******************************************************************************
     '******************************************************************************
@@ -100,13 +65,13 @@ Public Class GatewayUsuario
     ''' <param name="tipo">Tipo de usuario.</param>
     ''' <param name="observaciones">Dato adicional del usuario.</param>
     ''' <returns>Regresa el número de filas afectadas.</returns>
-    Public Function InsertarUsuario(nombre As String, apellidos As String, fechaNac As String, telefono As String, email As String, direccion As String, organizacion As String, tipo As String, observaciones As String) As Integer
+    Public Function InsertarUsuario(nombre As String, apellidos As String, fechaNac As String, telefono As String, email As String, direccion As String, organizacion As String, tipo As Integer, observaciones As String) As Integer
         Dim filasAfectadas As Integer
         Dim consulta As String
-        CampoVacio(nombre, "El campo nombre no puede estar vacío.")
-        CampoVacio(apellidos, "El campo apellidos no puede estar vacío.")
-        CampoVacio(fechaNac, "El campo fecha no puede estar vacío.")
-        CampoVacio(tipo, "El campo tipo no puede estar vacío.")
+        DatoNoValido(nombre.Trim(), "", "El campo nombre no puede estar vacío.")
+        DatoNoValido(apellidos.Trim(), "", "El campo apellidos no puede estar vacío.")
+        DatoNoValido(fechaNac.Trim(), "", "El campo fecha no puede estar vacío.")
+        DatoNoValido(tipo, 0, "El campo tipo no puede tener valor 0.")
         ContactoObligatorio(telefono, email)
         comando.Parameters.Add("@nombre", SqlDbType.VarChar)
         comando.Parameters("@nombre").Value = nombre
@@ -115,19 +80,19 @@ Public Class GatewayUsuario
         comando.Parameters.Add("@fechaNac", SqlDbType.Date)
         comando.Parameters("@fechaNac").Value = DateTime.Parse(fechaNac)
         comando.Parameters.Add("@telefono", SqlDbType.VarChar)
-        comando.Parameters("@telefono").Value = IIf(CampoEsNulo(telefono), DBNull.Value, telefono)
+        comando.Parameters("@telefono").Value = IIf(telefono.Trim().CompareTo("") = 0, DBNull.Value, telefono)
         comando.Parameters.Add("@correo", SqlDbType.VarChar)
-        comando.Parameters("@correo").Value = IIf(CampoEsNulo(email), DBNull.Value, email)
+        comando.Parameters("@correo").Value = IIf(email.Trim().CompareTo("") = 0, DBNull.Value, email)
         comando.Parameters.Add("@direccion", SqlDbType.VarChar)
-        comando.Parameters("@direccion").Value = IIf(CampoEsNulo(direccion), DBNull.Value, direccion)
+        comando.Parameters("@direccion").Value = IIf(direccion.Trim().CompareTo("") = 0, DBNull.Value, direccion)
         comando.Parameters.Add("@organizacion", SqlDbType.VarChar)
-        comando.Parameters("@organizacion").Value = IIf(CampoEsNulo(organizacion), DBNull.Value, organizacion)
+        comando.Parameters("@organizacion").Value = IIf(organizacion.Trim().CompareTo("") = 0, DBNull.Value, organizacion)
         comando.Parameters.Add("@tipo", SqlDbType.Int)
-        comando.Parameters("@tipo").Value = Integer.Parse(tipo)
+        comando.Parameters("@tipo").Value = tipo
         comando.Parameters.Add("@fechaAlta", SqlDbType.Date)
         comando.Parameters("@fechaAlta").Value = DateTime.Today
         comando.Parameters.Add("@observaciones", SqlDbType.Text)
-        comando.Parameters("@observaciones").Value = IIf(CampoEsNulo(observaciones), DBNull.Value, observaciones)
+        comando.Parameters("@observaciones").Value = IIf(observaciones.Trim().CompareTo("") = 0, DBNull.Value, observaciones)
         consulta = "INSERT INTO Usuarios ( nombre, apellidos, fecha_nacimiento, telefono, email, direccion, organizacion, tipo, fecha_alta, observaciones) VALUES (@nombre,@apellidos,@fechaNac,@telefono,@correo,@direccion,@organizacion,@tipo,@fechaAlta,@observaciones)"
         Try
             conexion.Open()
@@ -186,13 +151,13 @@ Public Class GatewayUsuario
     ''' <param name="tipo">Tipo de usuario.</param>
     ''' ''' <param name="observaciones">Dato adicional del usuario.</param>
     ''' <returns>Devuelve el número de filas afectadas.</returns>
-    Public Function ActualizarUsuario(id As Integer, nombre As String, apellidos As String, fechaNac As String, telefono As String, email As String, direccion As String, organizacion As String, tipo As String, observaciones As String) As Integer
+    Public Function ActualizarUsuario(id As Integer, nombre As String, apellidos As String, fechaNac As String, telefono As String, email As String, direccion As String, organizacion As String, tipo As Integer, observaciones As String) As Integer
         Dim consulta As String
         Dim filasAfectadas As Integer
-        CampoVacio(nombre, "El campo nombre no puede estar vacío.")
-        CampoVacio(apellidos, "El campo apellidos no puede estar vacío.")
-        CampoVacio(fechaNac, "El campo fecha no puede estar vacío.")
-        CampoVacio(tipo, "El campo tipo no puede estar vacío.")
+        DatoNoValido(nombre.Trim(), "", "El campo nombre no puede estar vacío.")
+        DatoNoValido(apellidos.Trim(), "", "El campo apellidos no puede estar vacío.")
+        DatoNoValido(fechaNac.Trim(), "", "El campo fecha no puede estar vacío.")
+        DatoNoValido(tipo, 0, "El campo tipo no puede no puede tener valor 0.")
         ContactoObligatorio(telefono, email)
         comando.Parameters.Add("@nombre", SqlDbType.VarChar)
         comando.Parameters("@nombre").Value = nombre
@@ -201,17 +166,17 @@ Public Class GatewayUsuario
         comando.Parameters.Add("@fechaNac", SqlDbType.Date)
         comando.Parameters("@fechaNac").Value = DateTime.Parse(fechaNac)
         comando.Parameters.Add("@telefono", SqlDbType.VarChar)
-        comando.Parameters("@telefono").Value = IIf(CampoEsNulo(telefono), DBNull.Value, telefono)
+        comando.Parameters("@telefono").Value = IIf(telefono.Trim().CompareTo("") = 0, DBNull.Value, telefono)
         comando.Parameters.Add("@correo", SqlDbType.VarChar)
-        comando.Parameters("@correo").Value = IIf(CampoEsNulo(email), DBNull.Value, email)
+        comando.Parameters("@correo").Value = IIf(email.Trim().CompareTo("") = 0, DBNull.Value, email)
         comando.Parameters.Add("@direccion", SqlDbType.VarChar)
-        comando.Parameters("@direccion").Value = IIf(CampoEsNulo(direccion), DBNull.Value, direccion)
+        comando.Parameters("@direccion").Value = IIf(direccion.Trim().CompareTo("") = 0, DBNull.Value, direccion)
         comando.Parameters.Add("@organizacion", SqlDbType.VarChar)
-        comando.Parameters("@organizacion").Value = IIf(CampoEsNulo(organizacion), DBNull.Value, organizacion)
+        comando.Parameters("@organizacion").Value = IIf(organizacion.Trim().CompareTo("") = 0, DBNull.Value, organizacion)
         comando.Parameters.Add("@tipo", SqlDbType.Int)
-        comando.Parameters("@tipo").Value = Integer.Parse(tipo)
+        comando.Parameters("@tipo").Value = tipo
         comando.Parameters.Add("@observaciones", SqlDbType.Text)
-        comando.Parameters("@observaciones").Value = IIf(CampoEsNulo(observaciones), DBNull.Value, observaciones)
+        comando.Parameters("@observaciones").Value = IIf(observaciones.Trim().CompareTo("") = 0, DBNull.Value, observaciones)
         consulta = "UPDATE Usuarios SET nombre=@nombre, apellidos=@apellidos, fecha_nacimiento=@fechaNac, telefono=@telefono, email=@correo, direccion=@direccion, organizacion=@organizacion, tipo=@tipo,observaciones=@observaciones WHERE id=" & id
         Try
             conexion.Open()
@@ -282,7 +247,7 @@ Public Class GatewayUsuario
         Dim consulta As String
         Dim resultado As New DataTable
         Dim lectura As SqlDataReader
-        CampoVacio(nombre, "El campo nombre no puede estar vacío.")
+        DatoNoValido(nombre.Trim(), "", "El campo nombre no puede estar vacío.")
         consulta = String.Format("SELECT * FROM usuarios WHERE nombre='{0}'", nombre)
         Try
             conexion.Open()
@@ -309,9 +274,7 @@ Public Class GatewayUsuario
         Dim consulta As String
         Dim resultado As New DataTable
         Dim lectura As SqlDataReader
-        If id = 0 Then
-            Throw New ArgumentException("El parámetro id no puede tener valor 0.")
-        End If
+        DatoNoValido(id, 0, "El campo id no puede tener valor 0,")
         consulta = "SELECT * FROM usuarios WHERE id=" & id
         Try
             conexion.Open()
--- ProyectoFablab/My Project/Application.Designer.vb
+++ ProyectoFablab/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ProyectoFablab.Form1
+            Me.MainForm = Global.ProyectoFablab.VentanaPrincipal
         End Sub
     End Class
 End Namespace
