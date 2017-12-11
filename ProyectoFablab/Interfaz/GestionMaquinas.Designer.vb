a58e02a5529529bbd8924a7dddb90c990c438d6f
alejandro <alejanfro.quiroga@iesdoctorbalmis.com>
2017-12-11 10:44:55 +0100

    nueva interfa maquinas

--- ProyectoFablab/Interfaz/GestionMaquinas.Designer.vb
+++ ProyectoFablab/Interfaz/GestionMaquinas.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form2
+Partial Class GestionMaquinas
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -24,20 +24,20 @@ Partial Class Form2
     Private Sub InitializeComponent()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Panel3 = New System.Windows.Forms.Panel()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
-        Me.Panel3.SuspendLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel3.SuspendLayout()
         Me.SuspendLayout()
         '
         'TableLayoutPanel1
@@ -68,6 +68,32 @@ Partial Class Form2
         Me.Panel1.Size = New System.Drawing.Size(861, 66)
         Me.Panel1.TabIndex = 0
         '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(331, 22)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 2
+        Me.Button5.Text = "Buscar"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(47, 22)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(267, 22)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "search"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(716, 17)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(119, 32)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "Nueva máquina"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
         'Panel2
         '
         Me.Panel2.Controls.Add(Me.DataGridView1)
@@ -76,6 +102,16 @@ Partial Class Form2
         Me.Panel2.Name = "Panel2"
         Me.Panel2.Size = New System.Drawing.Size(861, 330)
         Me.Panel2.TabIndex = 1
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(861, 330)
+        Me.DataGridView1.TabIndex = 0
         '
         'Panel3
         '
@@ -88,14 +124,14 @@ Partial Class Form2
         Me.Panel3.Size = New System.Drawing.Size(861, 66)
         Me.Panel3.TabIndex = 2
         '
-        'Button1
+        'Button3
         '
-        Me.Button1.Location = New System.Drawing.Point(552, 22)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(96, 27)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Consultar"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button3.Location = New System.Drawing.Point(756, 22)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(96, 27)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Elimiar"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'Button2
         '
@@ -106,50 +142,14 @@ Partial Class Form2
         Me.Button2.Text = "Editar"
         Me.Button2.UseVisualStyleBackColor = True
         '
-        'Button3
+        'Button1
         '
-        Me.Button3.Location = New System.Drawing.Point(756, 22)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(96, 27)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Elimiar"
-        Me.Button3.UseVisualStyleBackColor = True
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(861, 330)
-        Me.DataGridView1.TabIndex = 0
-        '
-        'Button4
-        '
-        Me.Button4.Location = New System.Drawing.Point(716, 17)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(119, 32)
-        Me.Button4.TabIndex = 0
-        Me.Button4.Text = "Nueva máquina"
-        Me.Button4.UseVisualStyleBackColor = True
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(47, 22)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(267, 22)
-        Me.TextBox1.TabIndex = 1
-        Me.TextBox1.Text = "search"
-        '
-        'Button5
-        '
-        Me.Button5.Location = New System.Drawing.Point(331, 22)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(75, 23)
-        Me.Button5.TabIndex = 2
-        Me.Button5.Text = "Button5"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.Button1.Location = New System.Drawing.Point(552, 22)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Consultar"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form2
         '
@@ -163,8 +163,8 @@ Partial Class Form2
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         Me.Panel2.ResumeLayout(False)
-        Me.Panel3.ResumeLayout(False)
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel3.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
--- ProyectoFablab/Interfaz/GestionMaquinas.vb
+++ ProyectoFablab/Interfaz/GestionMaquinas.vb
@@ -1,3 +1,3 @@
-﻿Public Class Form2
+﻿Public Class GestionMaquinas
 
 End Class
